3790da8a167d8cf8a2c00de6a588443094bb3539
Abhishek Gupta <142497451+abhishekgupta0803@users.noreply.github.com>
2025-05-04 15:15:42 +0530

    Update Hero.js

--- src/landing_page/about/Hero.js
+++ src/landing_page/about/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
         <div className='container mb-5 ' style={{marginTop:"5rem"}}>
             <div className='row text-center '>
                  <h1 className='fs-2 ' style={{color:"#424242"}}>We pioneered the discount broking model in India.<br/>
-                 Now, we are breaking ground with our technology.</h1>
+                 Now,we are breaking ground with our technology.</h1>
             </div>
         </div>
      );
